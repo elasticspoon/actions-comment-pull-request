4d707fb1b32370d0ed7135e144efce4e2acc4d76
elasticspoon <mail@yuribocharov.dev>
2024-10-13 01:45:36 -0400

    fixup!

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,13 @@ import * as github from '@actions/github';
 import * as core from '@actions/core';
 import { GetResponseDataTypeFromEndpointMethod } from '@octokit/types';
 
-// See https://docs.github.com/en/rest/reactions#reaction-types
-const REACTIONS = ['+1', '-1', 'laugh', 'confused', 'heart', 'hooray', 'rocket', 'eyes'] as const;
-type Reaction = (typeof REACTIONS)[number];
 
 async function run() {
   try {
     const message: string = core.getInput('message');
     const filePath: string = core.getInput('file-path');
     const githubToken: string = core.getInput('github-token');
-    const prNumber: string = core.getInput('pr-number');
     const commentTag: string = core.getInput('comment-tag');
-    const reactions: string = core.getInput('reactions');
     const mode: string = core.getInput('mode');
     const createIfNotExists: boolean = core.getInput('create-if-not-exists') === 'true';
 
@@ -23,44 +18,24 @@ async function run() {
       return;
     }
 
-
     let content: string = message;
     if (!message && filePath) {
       content = fs.readFileSync(filePath, 'utf8');
     }
 
     const context = github.context;
-    const issueNumber = parseInt(prNumber) || context.payload.pull_request?.number || context.payload.issue?.number;
+    const prNumber = context.payload.pull_request?.number;
     const prTitle = context.payload.pull_request?.title
     const titleRegex = new RegExp(`\\[GLOBAL-\\d*\\]`)
     if (!prTitle.match(titleRegex)) {
-      core.info("PR lint failed");
       content = "Add [GLOBAL-XXX] to your PR title to link up your Jira ticket\n" + content
     }
-    core.info(content);
 
     const octokit = github.getOctokit(githubToken);
 
-    if (!issueNumber) {
+    if (!prNumber) {
       core.setFailed('No issue/pull request in input neither in current context.');
       return;
-    }
-
-    async function addReactions(commentId: number, reactions: string) {
-      const validReactions = <Reaction[]>reactions
-        .replace(/\s/g, '')
-        .split(',')
-        .filter((reaction) => REACTIONS.includes(<Reaction>reaction));
-
-      await Promise.allSettled(
-        validReactions.map(async (content) => {
-          await octokit.rest.reactions.createForIssueComment({
-            ...context.repo,
-            comment_id: commentId,
-            content,
-          });
-        }),
-      );
     }
 
     async function createComment({
@@ -84,8 +59,6 @@ async function run() {
       core.setOutput('id', comment.id);
       core.setOutput('body', comment.body);
       core.setOutput('html-url', comment.html_url);
-
-      await addReactions(comment.id, reactions);
 
       return comment;
     }
@@ -112,18 +85,6 @@ async function run() {
       core.setOutput('body', comment.body);
       core.setOutput('html-url', comment.html_url);
 
-      await addReactions(comment.id, reactions);
-
-      return comment;
-    }
-
-    async function deleteComment({ owner, repo, commentId }: { owner: string; repo: string; commentId: number }) {
-      const { data: comment } = await octokit.rest.issues.deleteComment({
-        owner,
-        repo,
-        comment_id: commentId,
-      });
-
       return comment;
     }
 
@@ -137,62 +98,25 @@ async function run() {
       let comment: ListCommentsResponseDataType[0] | undefined;
       for await (const { data: comments } of octokit.paginate.iterator(octokit.rest.issues.listComments, {
         ...context.repo,
-        issue_number: issueNumber,
+        issue_number: prNumber,
       })) {
         comment = comments.find((comment) => comment?.body?.includes(commentTagPattern));
         if (comment) break;
       }
 
       if (comment) {
-        if (mode === 'upsert') {
-          await updateComment({
-            ...context.repo,
-            commentId: comment.id,
-            body,
-          });
-          return;
-        } else if (mode === 'recreate') {
-          await deleteComment({
-            ...context.repo,
-            commentId: comment.id,
-          });
-
-          await createComment({
-            ...context.repo,
-            issueNumber,
-            body,
-          });
-          return;
-        } else if (mode === 'delete') {
-          await deleteComment({
-            ...context.repo,
-            commentId: comment.id,
-          });
-          return;
-        } else if (mode === 'delete-on-completion') {
-          core.debug('Registering this comment to be deleted.');
-        } else {
-          core.setFailed(
-            `Mode ${mode} is unknown. Please use 'upsert', 'recreate', 'delete' or 'delete-on-completion'.`,
-          );
-          return;
-        }
-      } else if (mode === 'delete') {
-        core.info('No comment has been found with asked pattern. Nothing to delete.');
-        return;
-      } else if (createIfNotExists) {
-        core.info('No comment has been found with asked pattern. Creating a new comment.');
-      } else {
-        core.info(
-          'Not creating comment as the pattern has not been found. Use `create_if_not_exists: true` to create a new comment anyway.',
-        );
+        await updateComment({
+          ...context.repo,
+          commentId: comment.id,
+          body,
+        });
         return;
       }
     }
 
     await createComment({
       ...context.repo,
-      issueNumber,
+      issueNumber: prNumber,
       body,
     });
   } catch (error) {
